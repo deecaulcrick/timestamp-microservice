346c55573dfab896651e4c6cd145d90ef5a6cbc2
Dee Caulcrick <deecaulcrick@gmail.com>
2024-04-13 08:21:13 +0100

    fixed error checking

--- index.js
+++ index.js
@@ -19,16 +19,26 @@ app.get("/", function (req, res) {
 });
 
 // your first API endpoint...
-app.get("/api/:date", function (req, res) {
+app.get("/api/:date?", function (req, res) {
   const dateString = new Date(req.params.date);
   const timestamp = new Date(parseInt(req.params.date));
 
   const date = isNaN(req.params.date) ? dateString : timestamp;
 
-  if (isNaN(date.getTime())) {
-    return res.status(400).json({ error: "Invalid date" });
+  if (!req.params.date) {
+    const dateToday = new Date();
+    console.log(dateToday);
+    res.json({
+      unix: dateToday.valueOf(),
+      utc: dateToday.toUTCString(),
+    });
   }
 
+  console.log(dateString, timestamp);
+  if (date == "Invalid Date") {
+    res.status(400).json({ error: "Invalid Date" });
+    return;
+  }
   res.json({
     unix: dateString.valueOf() || timestamp.valueOf(),
     utc: date.toUTCString(),
